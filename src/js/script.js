5e69a29522bf296e8ec38abb817df09025645942
Carlos Alfaro Paniagua <alfaropaniagua313@gmail.com>
2024-12-12 21:10:16 +0100

    Get information about games of each genre

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ let routeCheapShark = 'https://www.cheapshark.com/api/1.0/deals?title='
 let rawgKey = "11a1d83c1a254e86a58245639677986c";
 let listGneres = document.getElementById('categorias');
 let contGames = document.getElementById('games');
+let cat = document.getElementById('categoria');
 //Funciones
 //Cargamos todos los tipos de generes
 const loadGenres = () =>{
@@ -33,12 +34,18 @@ const chooseCategorie = (event) =>{
 }
 //Cargamos los juegos dependiendo de la categoria que busque el usuario
 const gamesWithGenre = async (event) =>{
+    //Dejamos a vacio el contnedor
+    contGames.innerHTML = "";
+    //Sacamos la categoria que ha seleccionado el usuario
     let categoria = chooseCategorie(event);
-    let respuesta = await fetch(routeRawdGames+"key="+rawgKey+"&genres="+categoria);
+    //Le pasamos el tipo de categoria para completar el titulo
+    cat.textContent = categoria;
+    //Le pasamos la URL con la categoria
+    let respuesta = await fetch(routeRawdGames+"genres="+categoria.toLowerCase()+"&key="+rawgKey);
     let juegos = await respuesta.json();
     let juegosJSON = await juegos.results;
     console.log(juegosJSON);
-    //Cargamos los juegos
+    //Lllamamos a la funcion que carga los juegos
     showGames(juegosJSON)
 }
 //Mostramos los juegos que nos devuelve la API
@@ -48,14 +55,23 @@ const showGames = (games) =>{
     games.forEach((game) =>{
         //Contenedor
         let div = document.createElement('div');
-        //Titulo
-        let title = document.createElement('h3');
-        title.textContent = game.name;
-        div.appendChild(title);
         //Imagen
         let img = document.createElement('img');
         img.src = game.background_image;
         div.appendChild(img);
+        //Titulo
+        let title = document.createElement('h3');
+        title.textContent = game.name;
+        div.appendChild(title);
+        //Plataformas
+        let contPlataform = document.createElement('div');
+        game.platforms.forEach((plataforma) => {
+            let p = document.createElement('p');
+            p.textContent = plataforma.platform.name;
+            console.log(plataforma.platform.name);
+            contPlataform.appendChild(p);
+        });
+        div.appendChild(contPlataform);
         //Le pasamos el contenedor
         fragment.appendChild(div);
     });
