5f87707a665b1e3f2399083784eb1c11b2df54f7
Carlos Alfaro Paniagua <alfaropaniagua313@gmail.com>
2024-12-16 20:45:33 +0100

    Pagination in all offers

--- src/js/ofertas.js
+++ src/js/ofertas.js
@@ -1,33 +1,58 @@
 //Variables
-let routeCheapShark = 'https://www.cheapshark.com/api/1.0/deals';
+let routeCheapShark = 'https://www.cheapshark.com/api/1.0/deals?';
 let juego = document.getElementById('juego');
 let btn_enviar = document.getElementById('btn_enviar');
-let contAll = document.getElementById("all");
-let contUser = document.getElementById("especific");
+let contOferts = document.getElementById("oferts");
+let contPagina = document.getElementById('cont_pages');
+let numPagina = document.getElementById("num__page");
+let anterior = document.getElementById("btn_anterior");
+let siguiente = document.getElementById("btn_siguiente");
 //Funciones
+numPagina.textContent = 1;
+//Sacamos el numero de pagina
+const changeNumPage = (event) =>{
+    //Comprobamos que tipo de evento es y donde se hace
+    if (event.type == 'click' && event.target.tagName == "BUTTON") {
+        //Comprobamos en que boton se ha hecho
+        if (event.target == siguiente) {
+        numPagina.textContent++;
+        }else if (event.target == anterior && numPagina.textContent > 1) {
+        numPagina.textContent--;
+        }
+        //Comprobamos que juego se tiene que actualizas
+        if (juego.value.trim() === "") {
+            loadAllOferts(); 
+        }
+    }
+}
 //Cargamos todas las ofertas y mostramos
 const loadAllOferts = () =>{
-    fetch(routeCheapShark)
+    //Vaciamos el contenedor que de todas las ofertas
+    contOferts.innerHTML = "";
+    //Cargamos la pagina
+    fetch(routeCheapShark+"pageNumber="+numPagina.textContent)
     .then((ofertas) => ofertas.json())
     .then((ofertasJSON) => {
-        showOferts(ofertasJSON,contAll);
+        showOferts(ofertasJSON,contOferts);
     })
 }
 //Cargamos las ofertas del juego que busque el usuario por el buscados
 const loadEspecificOferts = async () =>{
-    //Ocultamos el contenedor que de todas las ofertas
-    contAll.style.display = "none";
+    //Vaciamos el contenedor que de todas las ofertas
+    contOferts.innerHTML = "";
+    //Ocultamos los ajustes de paginas
+    contPagina.style.display = "none";
     //Cargamos los datos
-    console.log(routeCheapShark+"?title="+juego.value);
-    let respuesta = await fetch(routeCheapShark+"?title="+juego.value);
+    let respuesta = await fetch(routeCheapShark+"title="+juego.value);
     let ofert = await respuesta.json();
     let datos = await ofert;
     //Cargamos los datos
-    showOferts(datos,contUser);
+    showOferts(datos,contOferts);
 }
 //Creamos los elementos HTML
 const showOferts = (array,contedor) =>{
     let fragment = new DocumentFragment();
+
     //Cargamos el array
     array.forEach((oferta) => {
         //Contenedor de cada card
@@ -50,16 +75,16 @@ const showOferts = (array,contedor) =>{
         contTexto.appendChild(titulo);
         //Contenedor precio
         let contPrecios = document.createElement("div");
-        //Precio normal
-        let normal = document.createElement("p");
-        normal.textContent = oferta.normalPrice + "€";
-        normal.classList.add("price__normal");
-        contPrecios.appendChild(normal);
         //Rebaja
         let rebaja = document.createElement("p");
         rebaja.textContent = oferta.salePrice + "€";
         rebaja.classList.add("sale__price")
         contPrecios.appendChild(rebaja);
+        //Precio normal
+        let normal = document.createElement("p");
+        normal.textContent = oferta.normalPrice + "€";
+        normal.classList.add("price__normal");
+        contPrecios.appendChild(normal);
         //Añadimos el contedor de precios
         contTexto.appendChild(contPrecios);
         //Boton compra
@@ -76,4 +101,6 @@ const showOferts = (array,contedor) =>{
 }
 //Eventos
 document.addEventListener('DOMContentLoaded',loadAllOferts);
-btn_enviar.addEventListener('click',loadEspecificOferts)
+btn_enviar.addEventListener('click',loadEspecificOferts);
+siguiente.addEventListener('click',changeNumPage);
+anterior.addEventListener('click',changeNumPage);
