f881273487c30a9966a26f458c6fd44aad185dcb
Carlos Alfaro Paniagua <alfaropaniagua313@gmail.com>
2024-12-27 17:28:49 +0100

    Rename page videogames and add new content

--- src/js/videojuegos.js
+++ src/js/videojuegos.js
@@ -4,6 +4,7 @@ let anterior = document.getElementById('btn_ant');
 let siguiente = document.getElementById('btn_sig');
 let contTitle = document.getElementById('title');
 let contTexto = document.getElementById('text')
+let stores = document.getElementById("stores");
 // Array de títulos
 const titulos = [
   "La revolución de los Battle Royale",
@@ -53,5 +54,53 @@ const changeGalery = (event) =>{
         }
     }
 }
+//Cargamos informacion sobre tiendas de videojuegos
+const storesGames = () =>{
+  fetch("https://www.cheapshark.com/api/1.0/stores")
+  .then((tiendas) => tiendas.json())
+  .then((tiendasJSON) => {
+    showStores(tiendasJSON);
+  })
+}
+//Estructura y estilo para los datos de las tiendas
+const showStores = (array) =>{
+  let fragment = new DocumentFragment();
+  //Recorremos el array
+  array.forEach((tienda) => {
+    //Contenedor
+    let cont = document.createElement('div');
+    cont.classList.add("card__store");
+    //Cotendor imgane
+    let contImage = document.createElement('div');
+    cont.appendChild(contImage);
+    //Imagen
+    let img = document.createElement('img');
+    img.src = "https://www.cheapshark.com" + tienda.images.logo;
+    contImage.appendChild(img);
+    //Contenedor texto
+    let contText = document.createElement('div');
+    contText.classList.add("card__store-text")
+    cont.appendChild(contText);
+    //Nombre tienda
+    let nombre = document.createElement("h3");
+    nombre.textContent = tienda.storeName;
+    contText.appendChild(nombre);
+    //Tienda activa
+    let activo = document.createElement('button');
+    if(tienda.isActive == 0){
+      activo.style.backgroundColor = "red";
+      activo.textContent = "No disponible";
+    }else if (tienda.isActive == 1) {
+      activo.style.backgroundColor = "green";
+      activo.textContent = "Disponible"
+    }
+    contText.appendChild(activo);
+    //Añadimos el contenedor al fragment
+    fragment.appendChild(cont);
+  });
+  //Añadimos el contenedor 
+  stores.appendChild(fragment);
+}
 //EVENTOS
-galeria.addEventListener('click',changeGalery)
+galeria.addEventListener('click',changeGalery);
+document.addEventListener('DOMContentLoaded',storesGames);
